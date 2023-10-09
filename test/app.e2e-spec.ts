96b9223731293eae6405f9384d8c13e6e49c4e3d
nic4cio <gabrielnn_1@live.com>
2023-10-08 23:59:25 -0300

    test db, test scripts and test environment variables

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,31 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
+import { CreateBookmarkDto, EditBookmarkDto } from '../src/bookmark/dto';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { EditUserDto } from '../src/user/dto';
 
 describe('App e2e', () => {
+  let app: INestApplication;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
   });
-  it.todo('should pass');
+
+  afterAll(() => {
+    app.close();
+  });
+
+  it.todo('should create a user');
 });
